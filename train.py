1a6990608a049eb2e66ddefb02fed1a2f939a196
Manukyan Davit <d.manukyan6139@gmail.com>
2024-03-21 21:13:54 +0400

    feat: report on every 10th batch

--- train.py
+++ train.py
@@ -13,52 +13,58 @@ from src.util.device import to_device
 summary_writer = SummaryWriter()
 
 
-def train_one_epoch(epoch_num, model, train_loader, preprocess_fn, optimizer, loss_fn):
+def train_one_epoch(epoch_num, model, train_loader, preprocess_fn, optimizer, loss_fn, batch_size):
     model.train()
-    epoch_loss = 0
     batch_running_loss = 0
     accurate_predictions = 0
-    num_examples = 0
     for index, batch in enumerate(train_loader):
         batch = to_device(batch)
         x, y = batch
-        num_examples += x.size(0)
         x = preprocess_fn(x)
         prediction = model(x)
-        accurate_predictions += (torch.argmax(prediction, dim=1) == torch.argmax(y, dim=1)).sum().item()
         loss = loss_fn(prediction, y)
-        epoch_loss += loss.item()
         batch_running_loss += loss.item()
+        accurate_predictions += (torch.argmax(prediction, dim=1) == torch.argmax(y, dim=1)).sum().item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if index % 10 == 9:
-            print(f'Batch: {index + 1}, loss: {batch_running_loss / 10}')
+        step = (epoch_num - 1) * len(train_loader) + index + 1
+        if step % 10 == 0:
+            report(batch_running_loss / 10, accurate_predictions / (10 * batch_size), step)
             batch_running_loss = 0
-    report('Training', epoch_loss / len(train_loader), accurate_predictions / num_examples, epoch_num)
+            accurate_predictions = 0
 
 
-def evaluate(epoch_num, model, test_loader, preprocess_fn, loss_fn):
+def evaluate(epoch_num, model, test_loader, preprocess_fn, loss_fn, batch_size):
     model.eval()
     test_loss = 0
     accurate_predictions = 0
-    num_examples = 0
     with torch.no_grad():
-        for batch in test_loader:
+        for index, batch in enumerate(test_loader):
             batch = to_device(batch)
             x, y = batch
-            num_examples += x.size(0)
             x = preprocess_fn(x)
             prediction = model(x)
-            accurate_predictions += (torch.argmax(prediction, dim=1) == torch.argmax(y, dim=1)).sum().item()
             loss = loss_fn(prediction, y)
             test_loss += loss.item()
-    report('Testing', test_loss / len(test_loader), accurate_predictions / num_examples, epoch_num)
+            accurate_predictions += (torch.argmax(prediction, dim=1) == torch.argmax(y, dim=1)).sum().item()
+            batch_num = index + 1
+            if batch_num % 10 == 0:
+                print(
+                    f'Epoch: {epoch_num}, '
+                    f'Batches: up to {batch_num}, '
+                    f'Test Loss: {test_loss / batch_num}, '
+                    f'Test Accuracy: {round(accurate_predictions / (batch_num * batch_size) * 100, 2)} %')
+    print(
+        f'Epoch: {epoch_num}, '
+        f'Overall Test Loss: {test_loss / len(test_loader)}, '
+        f'Overall Test Accuracy: {round(accurate_predictions / (batch_size * len(test_loader)) * 100, 2)} %')
 
 
-def report(phase, loss, accuracy, epoch):
-    summary_writer.add_scalar(f'{phase} loss', loss, epoch)
-    print(f'Phase: {phase}, Epoch: {epoch}, Loss: {loss}, Accuracy: {round(accuracy * 100, 2)} %')
+def report(loss, accuracy, step):
+    summary_writer.add_scalar(f'Train/Loss', loss, step)
+    summary_writer.add_scalar(f'Train/Accuracy', accuracy, step)
+    print(f'Training Step: {step}, Loss: {loss}, Accuracy: {round(accuracy * 100, 2)} %')
 
 
 def checkpoint(epoch_num, model, checkpoint_dir):
@@ -81,9 +87,11 @@ def main():
     loss_fn = torch.nn.CrossEntropyLoss(label_smoothing=args.label_smoothing)
 
     for epoch in range(1, args.epochs + 1):
-        train_one_epoch(epoch, model, train_loader, data_preprocessor, optimizer, loss_fn)
-        evaluate(epoch, model, test_loader, data_preprocessor, loss_fn)
+        train_one_epoch(epoch, model, train_loader, data_preprocessor, optimizer, loss_fn, args.batch_size)
         checkpoint(epoch, model, args.checkpoints_dir)
+        evaluate(epoch, model, test_loader, data_preprocessor, loss_fn, args.batch_size)
+
+    summary_writer.close()
 
 
 if __name__ == '__main__':
